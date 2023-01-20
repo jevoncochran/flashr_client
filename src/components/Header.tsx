2df46b524b221e73cb09aaf0657bd7dae7ccdaf1
Jevon Cochran <jevoncochran@Jevons-MacBook-Pro.local>
2023-01-20 09:23:14 -0500

    sign out functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
-import { useAppSelector } from "../app/hooks";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { logout } from "../features/auth/authSlice";
 import { Link } from "react-router-dom";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Button,
+} from "@chakra-ui/react";
 
 const Header = () => {
+  const dispatch = useAppDispatch();
+
   const { user } = useAppSelector((state) => state.auth);
 
   return (
@@ -20,22 +28,23 @@ const Header = () => {
           Decks
         </BreadcrumbLink>
       </BreadcrumbItem>
-      {!user ? (
-        <>
-          <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink as={Link} to="/login">
-              Login
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbItem isCurrentPage>
-            <BreadcrumbLink as={Link} to="/register">
-              Sign Up
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        </>
-      ) : (
-        <BreadcrumbItem isCurrentPage>
-          <BreadcrumbLink>Sign Out</BreadcrumbLink>
+      {!user && (
+        <BreadcrumbItem>
+          <BreadcrumbLink as={Link} to="/login">
+            Login
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      )}
+      {!user && (
+        <BreadcrumbItem>
+          <BreadcrumbLink as={Link} to="/register">
+            Sign Up
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      )}
+      {user && (
+        <BreadcrumbItem>
+          <Button onClick={() => dispatch(logout())}>Sign Out</Button>
         </BreadcrumbItem>
       )}
     </Breadcrumb>
