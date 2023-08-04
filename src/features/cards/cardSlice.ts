454322f9c2feb60acf38038494821fed644d65af
Jevon Cochran <jevon@securebags.com>
2023-08-03 19:00:20 -0500

    card swipe action

--- src/features/cards/cardSlice.ts
+++ src/features/cards/cardSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import cardService from "./cardService";
-import { DeckCard } from "../../Types";
+import { DeckCard, GuessResult } from "../../Types";
 
 interface CardState {
   cards: DeckCard[];
@@ -8,6 +8,13 @@ interface CardState {
   isError: boolean;
   isSuccess: boolean;
   message: string;
+}
+
+interface GuessResultData {
+  categoryId: string;
+  cardId: string;
+  cardInfo: DeckCard;
+  guessResult: GuessResult;
 }
 
 const initialState: CardState = {
@@ -24,6 +31,29 @@ export const getCards = createAsyncThunk(
     try {
       const token = thunkApi.getState().auth.user.token;
       return await cardService.getCards(categoryId, token);
+    } catch (error) {
+      const message =
+        (err.response && err.response.data && err.response.data.message) ||
+        err.message ||
+        err.toString();
+
+      return thunkApi.rejectWithValue(message);
+    }
+  }
+);
+
+export const logGuessResult = createAsyncThunk(
+  "/cards/logGuessResult",
+  async (guessResultData: GuessResultData, thunkApi) => {
+    try {
+      const token = thunkApi.getState().auth.user.token;
+      return await cardService.logGuessResult(
+        guessResultData.categoryId,
+        guessResultData.cardId,
+        guessResultData.cardInfo,
+        guessResultData.guessResult,
+        token
+      );
     } catch (error) {
       const message =
         (err.response && err.response.data && err.response.data.message) ||
--- src/types.ts
+++ src/types.ts
@@ -3,9 +3,12 @@ export interface DeckCard {
   front: string;
   back: string;
   archived: boolean;
+  consecutive_corrects: number | null;
 }
 
 export interface Category {
   id: string;
   title: string;
 }
+
+export type GuessResult = "correct" | "incorrect";
